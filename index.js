46074a9fe1bb884813ba12679334f3d533f66882
Ganikota Sreedhar <gandikotasreedhar98@gmail.com>
2021-06-25 00:18:13 +0530

    Task completed

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const taskContainer = document.querySelector(".task_container");
 
 // Global Store
 
-const globalStore = [];
+let globalStore = [];
 
 const newcard = ({id,imageUrl,taskTitle,taskDescription,taskType}) => 
 `<div class="col-md-6 col-lg-4 id=${id}">
 <div class="card">
   <div class="card-header d-flex justify-content-end gap-2">
     <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-    <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+    <button type="button id="${id}" class="btn btn-outline-danger onclick="deleteCard.apply(this, arguments)"><i class="fas fa-trash-alt" id="${id}" onclick="deleteCard.apply(this, arguments)"></i></button>
   </div>
   <img src=${imageUrl} class="card-img-top" alt="...">
   <div class="card-body">
@@ -42,6 +42,8 @@ const loadInitialTaskCards = () => {
   });
 }
 
+const updateLocalStorage = () => localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
+
 const saveChanges = () => {
     const taskData = {
         id: `${Date.now()}`, // unique number for card id
@@ -58,6 +60,45 @@ const saveChanges = () => {
     globalStore.push(taskData);
 
     // add to local storage
-    localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
+    updateLocalStorage();
 };
+
+const deleteCard = (event) => {
+  //id
+  
+  event = window.event;
+  const targetID = event.target.id;
+  const tagname = event.target.tagName;
+  //search the global, remove the object which matches with the id
+
+  const newUpdatedArray = globalStore.filter((cardObject) => cardObject.id != targetID);
+
+  // newUpdatedArray.map((cardObject) => {
+  //   const createNewCard = newcard(cardObject);
+  //   taskContainer.insertAdjacentHTML("beforeend",createNewCard);
+  // });
+
+  globalStore = newUpdatedArray;
+  updateLocalStorage();
+
+  // Accesss DOM to remove them
+
+  if(tagname === "BUTTON"){
+    // 
+    return taskContainer.removeChild(
+      event.target.parentNode.parentNode.parentNode // col-lg-4;
+    );
+  }
+    return taskContainer.removeChild(
+      event.target.parentNode.parentNode.parentNode.parentNode // col-lg-4;
+    );
+};
+
+// Features
+
+// Edit
+
+// Open
+
+
 
